284c739342a37b70b666673b6fcfdd76997fdf3b
pitassarii53-lab <pitassarii53@gmail.com>
2026-02-21 21:22:03 +0700

    Create privacy.js

---
+++ pages/privacy.js
@@ -0,0 +1,11 @@
+export default function Privacy() {
+  return (
+    <div style={{ padding: '40px', maxWidth: '800px', margin: 'auto', color: '#ccc', fontFamily: 'sans-serif', backgroundColor: '#111', minHeight: '100vh' }}>
+      <h1 style={{ color: '#fff' }}>Privacy Policy</h1>
+      <p>We use third-party advertising companies (Adsterra) to serve ads when you visit our website.</p>
+      <p>These companies may use information about your visits to this and other websites in order to provide advertisements about goods and services of interest to you.</p>
+      <p>By using this site, you agree to our use of cookies and other tracking technologies.</p>
+      <a href="/" style={{ color: '#f00' }}>Back to Home</a>
+    </div>
+  );
+}
